c70b5352aeb8c63763ad996f47cba5d62317b640
wanchaol <wanchaol@devvm3348.frc0.facebook.com>
2021-12-03 10:14:57 -0800

    [shard] add ShardedOptimizer
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/68607
    
    This PR added ShardedOptimizer and a API to get module parameters along with ShardedTensor param, it allows user to use this Optimizer Wrapper to construct a optimizer that involves ShardedTensor
    
    The state_dict support will be a follow up diff
    ghstack-source-id: 144720117
    
    Differential Revision: [D32539994](https://our.internmc.facebook.com/intern/diff/D32539994/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D32539994/)!

--- test/run_test.py
+++ test/run_test.py
@@ -204,6 +204,7 @@ WINDOWS_BLOCKLIST = [
     "distributed/_sharded_tensor/ops/test_embedding_bag",
     "distributed/_sharded_tensor/ops/test_init",
     "distributed/_sharded_tensor/ops/test_linear",
+    "distributed/_sharded_optim/test_sharded_optim",
 ] + FSDP_TEST
 
 ROCM_BLOCKLIST = [
@@ -216,6 +217,7 @@ ROCM_BLOCKLIST = [
     "distributed/_sharded_tensor/ops/test_embedding_bag",
     "distributed/_sharded_tensor/ops/test_init",
     "distributed/_sharded_tensor/ops/test_linear",
+    "distributed/_sharded_optim/test_sharded_optim",
     "test_determination",
     "test_multiprocessing",
     "test_jit_legacy",
@@ -353,6 +355,7 @@ DISTRIBUTED_TESTS = [
     "distributed/_sharded_tensor/ops/test_embedding_bag",
     "distributed/_sharded_tensor/ops/test_init",
     "distributed/_sharded_tensor/ops/test_linear",
+    "distributed/_sharded_optim/test_sharded_optim",
 ] + [test for test in TESTS if test.startswith("distributed/fsdp")]
 
 # Dictionary matching test modules (in TESTS) to lists of test cases (within that test_module) that would be run when
